b35be23b23695d5551908f30190d2d64a21d8170
Marcel Cardoso de Noronha <95543728+MarcelCNoronha@users.noreply.github.com>
2023-09-14 10:45:08 -0300

    rebase - correção página de exerciseManagement

--- trainsys/src/views/Exercises.vue
+++ trainsys/src/views/Exercises.vue
@@ -1,86 +1,114 @@
 <template>
-    <v-container>
-      <v-row justify="center">
-        <v-col cols="10">
-          <v-avatar size="100">
-            <v-icon size="100">mdi-account-supervisor</v-icon>
-          </v-avatar>
-          <v-card-title class="headline">Exercícios</v-card-title>
-        </v-col>
-      </v-row>
-  
-      <v-row justify="center">
-        <v-col cols="12">
-          <v-card>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="11" class="d-flex align-center">
+        <v-avatar size="100">
+          <v-icon size="100">mdi-dumbbell</v-icon>
+        </v-avatar>
+        <v-card-title class="headline">Exercícios</v-card-title>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-col cols="12">
+        <v-card>
+          <v-card-text>
             <v-form>
-              <v-text-field
-                v-model="novoItem"
-                label="Digite o nome do exercício"
-              ></v-text-field>
+              <v-row align="center" class="align-center">
+                <v-col cols="10">
+                  <v-text-field
+                    v-model="newExercise"
+                    label="Digite o nome do exercício"
+                    class="custom-text-field"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="2">
+                  <v-btn @click="addItem" color="primary" class="mx-auto">
+                    Cadastrar
+                  </v-btn>
+                </v-col>
+              </v-row>
             </v-form>
-            <v-card-actions>
-              <v-btn @click="adicionarItem" color="primary" class="mx-auto">
-                Cadastrar
-              </v-btn>
-            </v-card-actions>
-            <v-card-text>
-              <v-data-table :headers="headers" :items="itens" item-key="id">
-                <template v-slot:item.description="{ item }">
-                  {{ item.description }}
-                </template>
-              </v-data-table>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        novoItem: "",
-        itens: [],
-        headers: [
-          {
-            text: 'Descrição',
-            value: 'description'
-          }
-        ]
-      };
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-table>
+          <thead>
+            <tr>
+              <th class="text-left">Nome</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="exercises in exercises" :key="exercises.id">
+              <td>{{ exercises.description }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      newExercise: "",
+      exercises: [],
+    };
+  },
+  methods: {
+    addItem() {
+      const token = localStorage.getItem("exercises_token");
+
+      axios({
+        url: "http://localhost:3000/exercises",
+        method: "post",
+        data: {
+          description: this.newExercise,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then(() => {
+          alert("Cadastro com sucesso");
+          this.newExercise = "";
+        })
+        .catch(() => {
+          alert("Houve um erro ao realizar o cadastro");
+        });
+      this.loadExercises();
     },
-    methods: {
-      adicionarItem() {
-        if (this.novoItem.trim() !== "") {
-          axios.post('/api/adicionar-exercicio', { description: this.novoItem })
-            .then((response) => {
-              console.log(response.data);
-              this.itens.push({ description: this.novoItem });
-              this.novoItem = "";
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-        }
-      },
-      carregarExercicios() {
-        axios.get('/api/listar-exercicios')
-          .then((response) => {
-           
-            this.itens = response.data;
-          })
-          .catch((error) => {
-           
-            console.error(error);
-          });
-      },
+    loadExercises() {
+      const token = localStorage.getItem("exercises_token");
+
+      axios({
+        url: "http://localhost:3000/exercises",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => {
+          this.exercises = response.data;
+        })
+        .catch(() => {
+          alert("Ocorreu um erro ao buscar os exercícios");
+        });
     },
-    created() {
-      this.carregarExercicios();
-    },
-  };
-  </script>
-  
+  },
+  mounted() {
+    this.loadExercises();
+  },
+};
+</script>
+
+<style scoped>
+</style>
