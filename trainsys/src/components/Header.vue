aa1875f61127dc592d819138b1fc2cffa53dbfc5
Marcel Cardoso de Noronha <95543728+MarcelCNoronha@users.noreply.github.com>
2023-09-14 11:12:32 -0300

    refactor - corrigindo url menu

--- trainsys/src/components/Header.vue
+++ trainsys/src/components/Header.vue
@@ -2,8 +2,8 @@
     <v-app-bar app color="primary">
       <v-toolbar-title class="text-uppercase">TRAINSYS</v-toolbar-title>
       <v-btn text to="/dashboard">Home</v-btn>
-      <v-btn text to="/alunos">Alunos</v-btn>
-      <v-btn text to="/exercicios">Exercicios</v-btn>
+      <v-btn text to="/cadastro-alunos">Alunos</v-btn>
+      <v-btn text to="/cadastro-exercicio">Exercicios</v-btn>
       <v-btn text to="/sair">Sair</v-btn>
     </v-app-bar>
 </template>
