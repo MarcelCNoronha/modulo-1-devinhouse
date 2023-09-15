a205e1a0946f78e26df69a2197cc68d9ff73f2bc
Marcel Cardoso de Noronha <95543728+MarcelCNoronha@users.noreply.github.com>
2023-09-15 08:24:37 -0300

    feat: adição de botões montar treino e ver

--- trainsys/src/views/Dashboard.vue
+++ trainsys/src/views/Dashboard.vue
@@ -1,69 +1,67 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col cols="10">
+      <v-col cols="12">
         <v-card>
-          <v-card-title class="headline"
-            >Bem vindo, {{ usuarioLogado }}</v-card-title
-          >
+          <v-card-title class="headline">Bem vindo, {{ usuarioLogado }}</v-card-title>
         </v-card>
       </v-col>
     </v-row>
 
     <v-row justify="center">
-      <v-col cols="5">
-        <v-card>
-          <v-row>
-            <v-col cols="8">
-              <v-card-text class="d-flex align-center">
+      <v-col
+        :cols="isMobile ? 12 : 6"
+      >
+        <v-card class="pa-6">
+          <v-card-text>
+            <v-row>
+              <v-col cols="8">
                 <v-card-title class="headline">Alunos</v-card-title>
                 <div class="text-h2">{{ numeroAlunos }}</div>
-              </v-card-text>
-            </v-col>
-
-            <v-col cols="4">
-              <v-avatar size="80">
-                <v-icon size="80">mdi-account-supervisor</v-icon>
-              </v-avatar>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-card-actions>
+              </v-col>
+              <v-col cols="4">
+                <v-avatar size="80">
+                  <v-icon size="80">mdi-account-supervisor</v-icon>
+                </v-avatar>
+              </v-col>
+            </v-row>
+            <v-row>
               <router-link to="/cadastro-aluno">
                 <v-btn color="primary">Cadastrar Aluno</v-btn>
               </router-link>
-            </v-card-actions>
-          </v-row>
+            </v-row>
+          </v-card-text>
         </v-card>
       </v-col>
 
-      <v-col cols="5">
-        <v-card>
-          <v-row>
-            <v-col cols="8">
-              <v-card-text class="d-flex align-center">
+      <v-col
+        :cols="isMobile ? 12 : 6" 
+      >
+        <v-card class="pa-6">
+          <v-card-text>
+            <v-row>
+              <v-col cols="8">
                 <v-card-title class="headline">Exercícios</v-card-title>
                 <div class="text-h2">{{ numeroExercicios }}</div>
-              </v-card-text>
-            </v-col>
-            <v-col cols="4">
-              <v-avatar size="80">
-                <v-icon size="80">mdi-dumbbell</v-icon>
-              </v-avatar>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-card-actions>
+              </v-col>
+              <v-col cols="4">
+                <v-avatar size="80">
+                  <v-icon size="80">mdi-dumbbell</v-icon>
+                </v-avatar>
+              </v-col>
+            </v-row>
+            <v-row>
               <router-link to="/cadastro-exercicio">
                 <v-btn color="primary">Cadastrar Exercício</v-btn>
               </router-link>
-            </v-card-actions>
-          </v-row>
+            </v-row>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
 
 <script>
 import axios from "axios";
@@ -74,6 +72,7 @@ export default {
       usuarioLogado: "",
       numeroAlunos: 0,
       numeroExercicios: 0,
+      isMobile: false, 
     };
   },
 
@@ -96,6 +95,10 @@ export default {
   mounted() {
     this.loadDashboardData();
     this.usuarioLogado = localStorage.getItem("sessions_name");
+    this.isMobile = window.innerWidth < 600;
+    window.addEventListener("resize", () => {
+    this.isMobile = window.innerWidth < 600;
+  });
   },
 };
 </script>
--- trainsys/src/views/StudentList.vue
+++ trainsys/src/views/StudentList.vue
@@ -1,18 +1,16 @@
 <template>
   <v-container>
     <v-row justify="center">
-  <v-col cols="11" class="d-flex justify-space-between align-center">
-    <div class="d-flex align-center">
-      <v-avatar size="100">
-        <v-icon size="100">mdi-account-supervisor</v-icon>
-      </v-avatar>
-      <v-card-title class="headline ml-2">Alunos</v-card-title>
-    </div>
-    <v-btn to="/cadastro-aluno" color="primary">
-      Novo
-    </v-btn>
-  </v-col>
-</v-row>
+      <v-col cols="11" class="d-flex justify-space-between align-center">
+        <div class="d-flex align-center">
+          <v-avatar size="100">
+            <v-icon size="100">mdi-account-supervisor</v-icon>
+          </v-avatar>
+          <v-card-title class="headline ml-2">Alunos</v-card-title>
+        </div>
+        <v-btn to="/cadastro-aluno" color="primary"> Novo </v-btn>
+      </v-col>
+    </v-row>
     <v-row justify="center">
       <v-col cols="12">
         <v-card>
@@ -25,24 +23,36 @@
                 ></v-text-field>
               </v-col>
               <v-col cols="2" class="text-right">
-                <v-btn @click="filtrarAlunos" color="primary">
-                  Buscar
-                </v-btn>
+                <v-btn @click="filtrarAlunos" color="primary"> Buscar </v-btn>
               </v-col>
             </v-row>
-            <v-data-table :headers="headers" :items="alunosFiltrados" item-key="id">
-              <template v-slot:item.name="{ item }">
-                {{ item.name }}
-              </template>
-              <template v-slot:item.actions="{ item }">
-                <v-btn @click="redirecionarMontarTreino(item.id)" color="primary">
-                  Montar Treino
-                </v-btn>
-                <v-btn @click="redirecionarCadastroAluno" color="primary">
-                  Cadastrar Aluno
-                </v-btn>
-              </template>
-            </v-data-table>
+            <v-table>
+              <thead>
+                <tr>
+                  <th class="text-left">Nome</th>
+                </tr>
+              </thead>
+              <tbody>
+      <tr v-for="student in students" :key="student.id">
+        <td><h3>{{ student.name }}</h3></td>
+        <td class="text-right">
+          <v-btn
+            @click="redirecionarMontarTreino(student.id)"
+            color="primary"
+            class="mr-3"
+          >
+            Montar Treino
+          </v-btn>
+          <v-btn
+            @click="redirecionarAluno(student.id)"
+            color="primary"
+          >
+            Ver
+          </v-btn>
+        </td>
+      </tr>
+    </tbody>
+            </v-table>
           </v-card-text>
         </v-card>
       </v-col>
@@ -51,57 +61,50 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
-      alunos: [],
+      students: [],
       busca: "",
-      headers: [
-        {
-          text: 'Nome',
-          value: 'name'
-        },
-        {
-          text: 'Ações',
-          value: 'actions'
-        }
-      ]
     };
-  },
-  computed: {
-    alunosFiltrados() {
-      const termoBusca = this.busca.trim().toLowerCase();
-      return this.alunos.filter((aluno) => {
-        return aluno.name.toLowerCase().includes(termoBusca);
-      });
-    }
   },
   methods: {
     carregarAlunos() {
-      axios.get('/api/listar-alunos')
+      const students_token = localStorage.getItem("students_token");
+
+      axios({
+        url: "http://localhost:3000/students",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${students_token}`,
+        },
+      })
         .then((response) => {
-          this.alunos = response.data;
+          this.students = response.data.students;
         })
         .catch((error) => {
-          console.error(error);
+          alert("Ocorreu um erro ao buscar os alunos");
         });
     },
-    redirecionarMontarTreino(id) {
-      this.$router.push({ path: `/montar-treino/${id}` });
-    },
-    redirecionarCadastroAluno() {
-      this.$router.push('/cadastrar-aluno');
-    },
     filtrarAlunos() {
-      const termoBusca = this.busca.trim().toLowerCase();
-      this.alunosFiltrados = this.alunos.filter((aluno) => {
-        return aluno.name.toLowerCase().includes(termoBusca);
-      });
+      if (this.busca.trim() === "") {
+        this.carregarAlunos();
+      } else {
+        this.students = this.students.filter((student) =>
+          student.name.toLowerCase().includes(this.busca.toLowerCase())
+        );
+      }
+    },
+    redirecionarMontarTreino(id) {
+      this.$router.push({ path: `/workouts/${id}` });
+    },
+    redirecionarAluno(id) {
+      this.$router.push({ path: `/workouts/check/${id}` });
     },
   },
-  created() {
+  mounted() {
     this.carregarAlunos();
   },
 };
