b7e59c6e4bf74213b579c9c8cefef719494345b7
Marcel Cardoso de Noronha <95543728+MarcelCNoronha@users.noreply.github.com>
2023-09-16 21:30:35 -0300

    refactor - nome do aplicativo levando a página /dashboard

--- trainsys/src/components/Header.vue
+++ trainsys/src/components/Header.vue
@@ -1,10 +1,21 @@
 <template>
-    <v-app-bar app color="primary">
-      <v-toolbar-title class="text-uppercase">TRAINSYS</v-toolbar-title>
-      <v-btn text to="/dashboard">Home</v-btn>
-      <v-btn text to="/lista-alunos">Alunos</v-btn>
-      <v-btn text to="/cadastro-exercicio">Exercicios</v-btn>
-      <v-btn text to="/login">Sair</v-btn>
-    </v-app-bar>
+  <v-app-bar app color="primary">
+    <v-toolbar-title @click="navigateToDashboard" class="white--text">TRAINSYS</v-toolbar-title>
+
+    <v-btn text to="/dashboard">Home</v-btn>
+    <v-btn text to="/lista-alunos">Alunos</v-btn>
+    <v-btn text to="/cadastro-exercicio">Exercicios</v-btn>
+    <v-btn text to="/login">Sair</v-btn>
+  </v-app-bar>
 </template>
+
+<script>
+export default {
+  methods: {
+    navigateToDashboard() {
+      this.$router.push("/dashboard");
+    },
+  },
+};
+</script>
 
